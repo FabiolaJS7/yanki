cab964c2472d0ec427c445ee96f2d28af65503b6
Monica Fabiola Garcia Saravia <mgarcsar@emeal.nttdata.com>
2025-04-22 17:17:16 -0500

    checkstyle

--- yanki-service/src/main/java/com/yanki/service/service/MovementServiceImpl.java
+++ yanki-service/src/main/java/com/yanki/service/service/MovementServiceImpl.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class MovementServiceImpl implements MovementService{
+public class MovementServiceImpl implements MovementService {
 
     @Autowired
     KafkaProducer kafkaProducer;
