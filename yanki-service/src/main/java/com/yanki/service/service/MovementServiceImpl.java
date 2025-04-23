17eb70fa85a36fb2e85c68a7f5445dae57429fa3
Monica Fabiola Garcia Saravia <mgarcsar@emeal.nttdata.com>
2025-04-23 00:45:46 -0500

    corrigiendo bug

--- yanki-service/src/main/java/com/yanki/service/service/MovementServiceImpl.java
+++ yanki-service/src/main/java/com/yanki/service/service/MovementServiceImpl.java
@@ -4,10 +4,12 @@ import com.yanki.service.bean.MovementRequest;
 import com.yanki.service.producer.KafkaProducer;
 import com.yanki.service.util.JsonTransferUtil;
 import io.reactivex.rxjava3.core.Single;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class MovementServiceImpl implements MovementService {
 
     private static final String TOPIC_YANKI_MOVEMENT = "yanki-movement";
@@ -19,8 +21,15 @@ public class MovementServiceImpl implements MovementService {
 
     @Override
     public Single<String> createMovement(Single<MovementRequest> movementRequest) {
-        walletService.editWallet(movementRequest);
-        kafkaProducer.sendMessage(TOPIC_YANKI_MOVEMENT, JsonTransferUtil.objectToJson(movementRequest));
-        return Single.just("created");
+
+        return movementRequest
+                .flatMap(rq -> {
+                    walletService.editWallet(Single.just(rq));
+                    kafkaProducer.sendMessage(TOPIC_YANKI_MOVEMENT, JsonTransferUtil.objectToJson(rq));
+                    return Single.just("success");
+                })
+                .doOnSuccess(s -> log.info("Sending to create movement."))
+                .doOnError(throwable -> log.error("Sending to create movement error {}", throwable.getMessage()));
+
     }
 }
